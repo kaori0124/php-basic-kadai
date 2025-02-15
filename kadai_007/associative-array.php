5305a1574ddf61c7c55b1c1b3554f8fbef7ce1ed
kaori0124 <gyayukyo0124@gmail.com>
2025-02-15 10:02:02 +0900

    Add files via upload
    課題を提出します

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>課題：連想配列を作ってキーと値を出力しよう</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列に値を代入する
+       $price_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+       // 連想配列の値を出力する
+       print_r($price_data);
+
+       ?>
+   </p>
+</body>
+
+</html>
